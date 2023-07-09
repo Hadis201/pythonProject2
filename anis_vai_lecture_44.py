bfcc4891a742db12348be9bd08013d654e7629e0
88018 <mhrm201@gmail.com>
2023-07-09 19:31:40 +0600

    unsolved 001

---
+++ anis_vai_lecture_44.py
@@ -0,0 +1,10 @@
+file = open("txt2.txt", "w")
+file.write("\n105: Tasin Khalid")
+
+file = open("txt2.txt","a")
+file.write('\n106: Munim Muhtadi Jahin')
+file.close()
+
+file = open("hello.html", "w")
+file.write("this is new")
+file.close()
